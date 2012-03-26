dbdbcedd71ca7bb1434f20660227024d091284da
Ole Langbehn <ole.langbehn@googlemail.com>
2012-03-26 10:24:41 +0200

    Class Level annotation support.
    Class Level annotations are supported now. Inner classes and anonymous
    classes need to define their own class level annotations, there is no
    inheritance (this differs from findbugs, but solves ambiguity in case of
    inheritance).
    
    Transformed handling class level annotations via global state to
    traversing the AST with no state.

--- test/de/bjrke/checkstyle/jsr305/Jsr305AnnotationsTest.java
+++ test/de/bjrke/checkstyle/jsr305/Jsr305AnnotationsTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import de.bjrke.checkstyle.jsr305.Jsr305AnnationsTestUtil.ExpectedWarning;
 import de.bjrke.checkstyle.jsr305.test.ArraysTestObject;
+import de.bjrke.checkstyle.jsr305.test.ClassLevelReturnValueTestObject;
 import de.bjrke.checkstyle.jsr305.test.ConstructorTestObject;
 import de.bjrke.checkstyle.jsr305.test.DefectConstructorTest;
 import de.bjrke.checkstyle.jsr305.test.ParameterTestObject;
@@ -17,47 +18,61 @@ public class Jsr305AnnotationsTest {
         Jsr305AnnationsTestUtil.check(
 
         new ExpectedWarning( ParameterTestObject.class, 15, 45 ), //
-                new ExpectedWarning( ParameterTestObject.class, 20, 46 ), //
-                new ExpectedWarning( ParameterTestObject.class, 20, 63 ), //
-                new ExpectedWarning( ParameterTestObject.class, 50, 42 ), //
-                new ExpectedWarning( ParameterTestObject.class, 56, 28 ), // 
-                new ExpectedWarning( ParameterTestObject.class, 74, 36 ), //
+        new ExpectedWarning( ParameterTestObject.class, 20, 46 ), //
+        new ExpectedWarning( ParameterTestObject.class, 20, 63 ), //
+        new ExpectedWarning( ParameterTestObject.class, 50, 42 ), //
+        new ExpectedWarning( ParameterTestObject.class, 56, 28 ), // 
+        new ExpectedWarning( ParameterTestObject.class, 74, 36 ), //
 
-                new ExpectedWarning( PrimitivesTestObject.class, 10, 38 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 15, 39 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 20, 43 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 25, 5 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 31, 5 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 37, 5 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 43, 5 ), //
-                new ExpectedWarning( PrimitivesTestObject.class, 90, 35 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 10, 38 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 15, 39 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 20, 43 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 25, 5 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 31, 5 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 37, 5 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 43, 5 ), //
+        new ExpectedWarning( PrimitivesTestObject.class, 90, 35 ), //
 
-                new ExpectedWarning( ReturnValueTestObject.class, 29, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 35, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 56, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 63, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 83, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 86, 27 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 86, 44 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 91, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 98, 5 ), //
-                new ExpectedWarning( ReturnValueTestObject.class, 104, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 29, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 35, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 56, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 63, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 83, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 86, 27 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 86, 44 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 91, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 98, 5 ), //
+        new ExpectedWarning( ReturnValueTestObject.class, 104, 5 ), //
 
-                new ExpectedWarning( ConstructorTestObject.class, 19, 35 ), //
-                new ExpectedWarning( ConstructorTestObject.class, 25, 35 ), //
-                new ExpectedWarning( ConstructorTestObject.class, 39, 35 ), //
-                new ExpectedWarning( ConstructorTestObject.class, 53, 53 ), //
+        new ExpectedWarning( ConstructorTestObject.class, 19, 35 ), //
+        new ExpectedWarning( ConstructorTestObject.class, 25, 35 ), //
+        new ExpectedWarning( ConstructorTestObject.class, 39, 35 ), //
+        new ExpectedWarning( ConstructorTestObject.class, 53, 53 ), //
 
-                new ExpectedWarning( DefectConstructorTest.class ), //
+        new ExpectedWarning( DefectConstructorTest.class ), //
 
-                new ExpectedWarning( ArraysTestObject.class, 41, 23 ), //
-                new ExpectedWarning( ArraysTestObject.class, 56, 23 ), //
+        new ExpectedWarning( ArraysTestObject.class, 41, 23 ), //
+        new ExpectedWarning( ArraysTestObject.class, 56, 23 ), //
 
-                new ExpectedWarning( ArraysTestObject.class, 71, 23 ), //
-                new ExpectedWarning( ArraysTestObject.class, 86, 23 ), //
-                new ExpectedWarning( ArraysTestObject.class, 100, 8 ), //
-                new ExpectedWarning( ArraysTestObject.class, 115, 33 ), //
-                new ExpectedWarning( ArraysTestObject.class, 130, 35 ) //
+        new ExpectedWarning( ArraysTestObject.class, 71, 23 ), //
+        new ExpectedWarning( ArraysTestObject.class, 86, 23 ), //
+        new ExpectedWarning( ArraysTestObject.class, 100, 8 ), //
+        new ExpectedWarning( ArraysTestObject.class, 115, 33 ), //
+        new ExpectedWarning( ArraysTestObject.class, 130, 35 ), //
+
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 14, 9 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 28, 45 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 38, 39 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 58, 48 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 65, 39 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 109, 36 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 115, 17 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 119, 17 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 130, 17 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 136, 17 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 154, 17 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 189, 33 ),
+		new ExpectedWarning( ClassLevelReturnValueTestObject.class, 224, 41 )
 
         );
     }
---
+++ test/de/bjrke/checkstyle/jsr305/test/ClassLevelReturnValueTestObject.java
@@ -0,0 +1,229 @@
+package de.bjrke.checkstyle.jsr305.test;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.ParametersAreNullableByDefault;
+
+import edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault;
+
+public class ClassLevelReturnValueTestObject {
+
+	// error, double annotation
+	@ParametersAreNonnullByDefault
+	@ParametersAreNullableByDefault
+	class BothParameterAnnotationsClass {
+		
+	}
+	
+	// no error
+	@ParametersAreNullableByDefault
+	interface ParameterAnnotationsInterface {
+		
+		// no error
+		public void setAValue(String foo);
+		
+		// error, redundant
+		public void setAnotherValue(@Nullable String foo);
+		
+		// no error
+		public void setYetAnotherValue(@Nonnull String foopoo);
+		
+	}
+	
+	interface NoParameterAnnotationsInterface {
+		
+		// error, missing
+		public void setAValue(String foo);
+		
+		// no error
+		public void setAnotherValue(@Nonnull String foo);
+		
+		// no error
+		public void setYetAnotherValue(@Nullable String foo);
+		
+	}
+	
+	@ParametersAreNonnullByDefault
+	class ParameterAnnotationsClass {
+		
+		// no error
+		public void setAValue(String foo){}
+		
+		// no error
+		public void setAnotherValue(@Nullable String foo){}
+		
+		// error, redundant
+		public void setYetAnotherValue(@Nonnull String foo){}
+		
+	}
+	
+	class NoParameterAnnotationsClass {
+		
+		// error, missing
+		public void setAValue(String foo){}
+		
+		// no error
+		public void setAnotherValue(@Nonnull String foo){}
+		
+		// no error
+		public void setYetAnotherValue(@Nullable String foo){}
+		
+	}
+	
+	@ParametersAreNonnullByDefault
+	static class DefaultNullableParameterInheritingClass implements ParameterAnnotationsInterface {
+		
+		@Override
+		// no error
+		public void setAValue(String foo) {}
+
+		@Override
+		// no error
+		public void setAnotherValue(String foo) {}
+
+		@Override
+		// no error
+		public void setYetAnotherValue(String foopoo) {}
+		
+		// no error
+		public void setFoo(String foo){}
+	}
+
+	static class NullableParameterInheritingClass implements ParameterAnnotationsInterface {
+		
+		@Override
+		// no error
+		public void setAValue(String foo) {}
+
+		@Override
+		// no error
+		public void setAnotherValue(String foo) {}
+
+		@Override
+		// no error
+		public void setYetAnotherValue(String foopoo) {}
+		
+		// error
+		public void setFoo(String foo){}
+	}
+	
+	@ReturnValuesAreNonnullByDefault
+	interface ReturnValueAnnotationInterface {
+		// error, redundant
+		@Nonnull
+		public String getAValue();
+
+		// error, disallowed
+		@ReturnValuesAreNonnullByDefault
+		public String getYetAnotherValue();
+
+		// no error
+		@CheckForNull
+		public String getAnotherValue();
+	}
+
+	@ReturnValuesAreNonnullByDefault
+	static class ReturnValueAnnotationClass {
+		// error, redundant
+		@Nonnull
+		public String getAValue() {
+			return "";
+		}
+
+		// error, disallowed
+		@ReturnValuesAreNonnullByDefault
+		public String getYetAnotherValue() {
+			return "";
+		}
+
+		// no error
+		@CheckForNull
+		public String getAnotherValue() {
+			return "";
+		}
+	}
+
+	interface NoReturnValueAnnotationInterface {
+		// no error
+		@Nonnull
+		public String getAValue();
+
+		// error, disallowed
+		@ReturnValuesAreNonnullByDefault
+		public String getYetAnotherValue();
+
+		// no error
+		@CheckForNull
+		public String getAnotherValue();
+	}
+
+}
+
+@ParametersAreNullableByDefault
+class InheritanceTest {
+
+	@ParametersAreNonnullByDefault
+	interface Foo {
+		
+		// no error
+		void foo(String bar);
+		
+		// no error
+		void bar(@Nullable String foo);
+	}
+	
+	// no error
+	static class Fop implements Foo{
+	
+		// no error, since we're overriding @NonnullByDefault from the interface
+		@Override
+		public void foo(@Nullable String bar) {}
+
+		// no error
+		@Override
+		public void bar(String foo) {}
+		
+		// error
+		public void baz(String foo) {}
+		
+	}
+	
+	// no error
+	@ParametersAreNullableByDefault
+	static class FopToo implements Foo{
+	
+		// this should not be an error, we are inheriting via the @Override annotation
+		@Override
+		public void foo(String bar) {}
+		
+		// no error
+		@Override
+		public void bar(String baz) {}
+		
+		// no error
+		public void baz(String foo) {}
+		
+	}
+	
+	// anonymous classes can't be annotated, but this should inherit the annotations from the superclass
+	public void testAnonClasses() {
+		@SuppressWarnings("unused")
+		Foo foo = new Foo() {
+			
+			// no error
+			@Override
+			public void foo(String barrrrrr) {}
+
+			// no error
+			@Override
+			public void bar(String foo) {}
+			
+			// error
+			public void baz(String foo) {}
+		};
+	}
+
+	
+}
